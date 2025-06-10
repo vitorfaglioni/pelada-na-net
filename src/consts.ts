e29d8d81c1d343dce9674f484e05f0db115fe0a8
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2025-06-10 17:14:03 -0300

    consts changed

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Pelada na Net';
-export const SITE_DESCRIPTION = 'Um podcast por Príncipe Vidane';
+export const SITE_DESCRIPTION = 'Um podcast de Príncipe Vidane';
